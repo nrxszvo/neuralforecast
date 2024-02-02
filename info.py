43ccd486ecd8589459707c7100915c61735d7315
Michael Horgan <michael.g.horgan@gmail.com>
2024-02-02 14:18:18 -0800

    info improvements

--- info.py
+++ info.py
@@ -2,7 +2,7 @@ import os
 import re
 import numpy as np
 import matplotlib.pyplot as plt
-from scipy.stats import norm
+from scipy.stats import expon
 
 
 def load(fn):
@@ -10,12 +10,12 @@ def load(fn):
 
 
 def get_ys(d):
-    yt = d["y_true"][0]
-    yh = d["y_hat"][0]
+    yt = d["y_true"]
+    yh = d["y_hat"]
     return yt, yh
 
 
-def calc_mae(d, ax=(0, 1, 2)):
+def calc_mae(d, ax=(0, 1, 2, 3)):
     yt, yh = get_ys(d)
     mae = np.abs(yt - yh)
     if len(ax) > 0:
@@ -23,7 +23,7 @@ def calc_mae(d, ax=(0, 1, 2)):
     return mae
 
 
-def calc_smape(d, ax=(0, 1, 2)):
+def calc_smape(d, ax=(0, 1, 2, 3)):
     yt, yh = get_ys(d)
     t = np.prod([yt.shape[i] for i in ax])
     smape = (2 / t) * np.sum(np.abs(yt - yh) / (np.abs(yt) + np.abs(yh)), axis=ax)
@@ -34,16 +34,24 @@ def plot_dist(ds):
     ax1 = plt.figure().add_subplot()
     ax2 = plt.figure().add_subplot()
     for d in ds:
-        err = calc_smape(d, ax=(1, 2))
-        mu, std = norm.fit(err)
-        ax1.hist(err, bins=100, density=True, alpha=0.6, label=d["series"][0])
-        xmin, xmax = ax1.get_xlim()
-        x = np.linspace(xmin, xmax, 100)
-        p = norm.pdf(x, mu, std)
-        ax1.plot(x, p, "k", linewidth=2)
-        ax2.plot(err, label=d["series"][0])
-
-    ax1.set_title(", ".join([d["series"][0] for d in ds]) + "-- error distribution")
+        err = calc_smape(d, ax=(2, 3))
+        for s in range(err.shape[0]):
+            # mu, std = expon.fit(err)
+            ax1.hist(err[s], bins=100, alpha=0.6, label=d["series"][s])
+            # xmin, xmax = ax1.get_xlim()
+            # x = np.linspace(xmin, xmax, 100)
+            # p = expon.pdf(x, mu, std)
+            # ax1.plot(x, p, "k", linewidth=2)
+            ax2.scatter(np.arange(len(err[s])), err[s], s=0.5, label=d["series"][s])
+    ax1.set_xlabel("sMAPE")
+    ax1.set_ylabel("# windows")
+    ax1.legend()
+    ax2.set_xlabel("window index")
+    ax2.set_ylabel("sMAPE")
+    ax2.legend()
+    ax1.set_title(
+        ", ".join([", ".join(d["series"]) for d in ds]) + "-- error distribution"
+    )
     ax2.set_title("Error by window")
     plt.show()
     plt.close()
@@ -70,10 +78,10 @@ def plot_summary_menu(available):
         plot_choices_menu(available, plot_summary)
 
 
-def plot_3d(d, idx):
+def plot_3d(d, sidx, widx):
     ax = plt.figure().add_subplot(projection="3d")
-    ax.plot(*d["y_true"][0, idx].T, label="y_true")
-    ax.plot(*d["y_hat"][0, idx].T, label="y_hat")
+    ax.plot(*d["y_true"][sidx, widx].T, label="y_true")
+    ax.plot(*d["y_hat"][sidx, widx].T, label="y_hat")
     plt.show()
     plt.close()
 
@@ -124,13 +132,17 @@ def plot_3d_menu(available):
             else:
                 try:
                     d = load(available[int(sidx)][1])
+                    nser = d["y_true"].shape[0]
                     nwin = d["y_true"].shape[1]
                     while True:
-                        widx = input(f"window to plot [0, {nwin}] (q to quit): ")
-                        if widx == "q":
+                        resp = input(
+                            f"series and window to plot [0, {nser-1}].[0, {nwin-1}] (q to quit): "
+                        )
+                        if resp == "q":
                             break
                         else:
-                            plot_3d(d, int(widx))
+                            sidx, widx = resp.split(".")
+                            plot_3d(d, int(sidx), int(widx))
                 except Exception as e:
                     print(e)
         except Exception as e:
