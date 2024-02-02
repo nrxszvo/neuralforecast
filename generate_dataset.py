501d4cfa0c968c9c5436a092b2aae3f5aa3d39b6
Michael Horgan <michael.g.horgan@gmail.com>
2024-02-02 19:42:13 +0000

    tweaks to dataset generation script

--- generate_dataset.py
+++ generate_dataset.py
@@ -9,13 +9,17 @@ import dysts.flows as flows
 import json
 
 
-def make_randic(name, nseq, seqlen, ic_perturb):
-    fn = f"{name}_{nseq}x{seqlen}_ic-{ic_perturb}"
+def make_multi_ic(name, n_ic, seqlen, ic_perturb, fn=None):
+    if fn is None:
+        fn = f"{name}_{n_ic}x{seqlen}_ic-{ic_perturb}"
     if name == "Lorenz":
         model = flows.Lorenz()
     else:
         raise Exception
-    perturb = 1 + (ic_perturb / 2) - ic_perturb * np.random.random((nseq, 3))
+    model_md = model._load_data()
+    ndim = model_md["embedding_dimension"]
+    perturb = 1 + ic_perturb - (2 * ic_perturb) * np.random.random((n_ic, ndim))
+    perturb[0] = np.ones(ndim)  # first ic are defaults
     model.ic = model.ic[None, :] * perturb
     tpts, sol = model.make_trajectory(n=seqlen, return_times=True)
 
@@ -23,11 +27,18 @@ def make_randic(name, nseq, seqlen, ic_perturb):
         {"unique_id": str, "ds": np.int64, "y": np.float64}
     )
 
-    md = {"ic": model.ic.tolist(), "dt": tpts[1]}
+    md = {
+        "model": name,
+        "ndim": ndim,
+        "dt": tpts[1],
+        "lyapunov_time": 1 / model_md["maximum_lyapunov_estimated"],
+        "period": model_md["period"],
+        "ic": model.ic.tolist(),
+    }
     with open(f"{fn}.json", "w") as f:
         json.dump(md, f, sort_keys=True, indent=4)
 
-    for i in range(nseq):
+    for i in range(n_ic):
         y = sol[i].reshape(-1)
         ds = np.arange(len(y))
         uid = [f"{name}-{i}"] * len(y)
@@ -37,7 +48,7 @@ def make_randic(name, nseq, seqlen, ic_perturb):
     df.to_csv(f"{fn}.csv", index=False)
 
 
-def make(seqlen=100, pts_per_period=75, resample=True):
+def make_ensemble(seqlen=100, pts_per_period=75, resample=True):
     csvfn = f"dysts_ensemble_3d-{seqlen}.csv"
     if os.path.exists(csvfn):
         print(f"reading from {csvfn}")
@@ -64,8 +75,27 @@ def make(seqlen=100, pts_per_period=75, resample=True):
 
 
 if __name__ == "__main__":
-    import sys
+    import argparse
 
-    # seqlen = int(sys.argv[1])
-    # make(seqlen)
-    make_randic("Lorenz", 25, 10000, 0.02)
+    parser = argparse.ArgumentParser(
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter
+    )
+    parser.add_argument(
+        "--model", default="Lorenz", help="model name", choices=["Lorenz"]
+    )
+    parser.add_argument("--seqlen", default=10000, help="sequence length", type=int)
+    parser.add_argument(
+        "--ic_perturb",
+        default=0.01,
+        help="percent random perturbation of initial conditions",
+        type=float,
+    )
+    parser.add_argument(
+        "--num_ic",
+        default=100,
+        help="number of unique initial conditions to generate",
+        type=int,
+    )
+    parser.add_argument("--fn", default=None, help="output filename")
+    args = parser.parse_args()
+    make_multi_ic(args.model, args.num_ic, args.seqlen, args.ic_perturb)
